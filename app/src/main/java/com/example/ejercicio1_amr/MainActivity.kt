30efc71a96e5a8390e1f6a5074be90e24517b684
adolf <adolf96.mr@gmail.com>
2022-11-20 00:34:10 -0600

    commit inicial

---
+++ app/src/main/java/com/example/ejercicio1_amr/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ejercicio1_amr
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ejercicio1_amr.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun click(view: View) {}
+}
